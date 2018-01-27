b1395a8be310643a0d418045dd65cdb5e7df533e
Dwight Goins <dngoins@hotmail.com>
2018-01-27 17:36:42 -0500

    Fixed working sample

--- C++/Rendering3DText/Common/DirectXHelper.h
+++ C++/Rendering3DText/Common/DirectXHelper.h
@@ -4,6 +4,49 @@
 
 namespace DX
 {
+	class com_exception : public std::exception
+	{
+	public:
+		com_exception(HRESULT hr) : result(hr) {}
+
+		virtual const char* what() const override
+		{
+			static char s_str[64] = {};
+			sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
+			return s_str;
+		}
+
+	private:
+		HRESULT result;
+	};
+
+	// Helper utility converts D3D API failures into exceptions.
+	inline void ThrowIfFailed(HRESULT hr, bool use_standard )
+	{
+		if (FAILED(hr))
+		{
+			throw com_exception(hr);
+		}
+	}
+
+
+	// Helper for output debug tracing
+	inline void DebugTrace(_In_z_ _Printf_format_string_ const char* format, ...)
+	{
+#ifdef _DEBUG
+		va_list args;
+		va_start(args, format);
+
+		char buff[1024] = {};
+		vsprintf_s(buff, format, args);
+		OutputDebugStringA(buff);
+		va_end(args);
+#else
+		UNREFERENCED_PARAMETER(format);
+#endif
+	}
+
+
     inline void ThrowIfFailed(HRESULT hr)
     {
         if (FAILED(hr))
